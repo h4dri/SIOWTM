fd6befcfd33325cca0edb0b5791b37f4350cb570
h4dri <adrianmalik333@gmail.com>
2021-01-13 22:58:13 +0100

    200ok

--- Application/Visits/Edit.cs
+++ Application/Visits/Edit.cs
@@ -43,32 +43,30 @@ namespace Application.Visits
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            private readonly IMapper _mapper;
-            public Handler(DataContext context, IMapper mapper)
+            public Handler(DataContext context)
             {
-                _mapper = mapper;
                 _context = context;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var visit = await _context.Visits.FindAsync(request.Id);
+                        if(visit == null)
+                            throw new RestException(HttpStatusCode.NotFound,
+                            new {visit = "Not Found"});
+                        
+                        visit.Title = request.Title ?? visit.Title;
+                        visit.Description = request.Description ?? visit.Description;
+                        visit.Category = request.Category ?? visit.Category;
+                        visit.Date = request.Date ?? visit.Date;
+                        visit.DocName = request.DocName ?? visit.DocName;
+                        visit.isEnded = request.isEnded;
 
-                if (visit == null)
-                    throw new RestException(HttpStatusCode.NotFound, new { Visit = "Not found" });
-
-                visit.Title = request.Title ?? visit.Title;
-                visit.Description = request.Description ?? visit.Description;
-                visit.Category = request.Category ?? visit.Category;
-                visit.Date = request.Date ?? visit.Date;
-                visit.DocName = request.DocName ?? visit.DocName;
-                visit.isEnded = request.isEnded;
-
-                var success = await _context.SaveChangesAsync() > 0;
-
-                if (success) return Unit.Value;
-
-                throw new Exception("Problem saving changes");
+                        var success = await _context.SaveChangesAsync() > 0;
+        
+                        if(success) return Unit.Value;
+        
+                        throw new Exception("Problem saving changes");
             }
         }
     }
